af52726ec4fc0e1f01e57499dc07365025395687
Jonathan Julião <jon.m.juliao@gmail.com>
2025-11-22 16:50:50 -0300

    feat: adiciona CandidatoService

--- src/main/java/com/example/demo/repository/CandidatoRepository.java
+++ src/main/java/com/example/demo/repository/CandidatoRepository.java
@@ -3,10 +3,11 @@ package com.example.demo.repository;
 import com.example.demo.model.CPF;
 import com.example.demo.model.Candidato;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface CandidatoRepository extends JpaRepository<Candidato, Long> {
 
     boolean existsByCpf(CPF cpf);
