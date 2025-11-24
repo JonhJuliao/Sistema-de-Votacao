04090389cd418def28876bc242575dc77d15dfae
Guilherme <bonifacioguilherme1@gmail.com>
2025-11-24 10:55:28 -0300

    feat: implementar atributo jaVotou e métodos associados (GREEN)

--- src/main/java/com/example/demo/model/Eleitor.java
+++ src/main/java/com/example/demo/model/Eleitor.java
@@ -5,11 +5,23 @@ import jakarta.persistence.Entity;
 @Entity
 public class Eleitor extends Pessoa {
 
+    private boolean jaVotou;
+
     public Eleitor() {
         super();
+        this.jaVotou = false;
     }
 
     public Eleitor(String nome, CPF cpf) {
-        super(cpf, nome, 18); // usar uma idade padrão (mínimo para votar)
+        super(cpf, nome, 18); // usa idade mínima
+        this.jaVotou = false;
+    }
+
+    public boolean isJaVotou() {
+        return jaVotou;
+    }
+
+    public void setJaVotou(boolean jaVotou) {
+        this.jaVotou = jaVotou;
     }
 }
